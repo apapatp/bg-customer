3960602f575daa5ad2d17c145eb480556875c5c0
apapatp <apapatp@gmail.com>
2021-05-04 00:58:54 -0500

    First commit

---
+++ AppUser/www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={						
+	'ApiUrl':"https://www.bargaingrub.com/mobileappv2/api",
+	'AppTitle':"Bargain Grub",
+	'ApiKey' : 'ab026d43d72373ea59773bc8812fdd9f2',	
+ 	'LicenseKey': '4e3a60b390a77c3b3d2bba47d7fb8b2b',
+	'debug': false
+};
